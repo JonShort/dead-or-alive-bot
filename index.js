5aa19cbebf6edba80ec89f0a25823d565199f83e
Weiran Zhang <weiran@zhang.me.uk>
2017-09-02 10:23:25 +0100

    Removed usused param

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Composer } = require('micro-bot');
 const BotRunner = require('./BotRunner');
 
-const bot = new Composer(process.env.BOT_TOKEN);
+const bot = new Composer();
 bot.on('text', BotRunner.textReceived);
 
 module.exports = bot;
